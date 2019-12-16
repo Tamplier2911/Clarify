8efbb951e3094890c8192226fd7fd728b865476b
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2019-12-17 01:25:26 +0200

    Small considirations.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,45 @@
-const express = require("express");
-const path = require("path");
+const dotenv = require("dotenv");
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+process.on("uncaughtException", err => {
+  console.log("UNCAUGHT EXCEPTION", err.name, err.message);
+  process.exit(1);
 });
 
-module.exports = app;
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+// DB CONNECTION
+/*
+const database = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('DB Connection Successful!');
+  });
+
+*/
+
+// SERVER
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}/ ...`);
+});
+
+process.on("unhandledRejection", err => {
+  console.log("UNDANDLED REJECTION", err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM RECIEVED. Shutting down...");
+  server.close(() => {
+    console.log("Process terminated...");
+  });
+});
