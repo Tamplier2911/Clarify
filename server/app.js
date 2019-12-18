6d406e7495b76a3489711aeb1c53965024f8b856
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2019-12-18 10:20:25 +0200

    Installed passport js lib, half way through implementation of google oauth.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,44 @@
+// express app
 const express = require("express");
+
+// authentication process
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+// pathfinder
 const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// make passport to take use of new instance of google strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_OAUTH_PUBLIC,
+      clientSecret: process.env.GOOGLE_OAUTH_SECRET,
+      callbackURL: "/auth/google/cb"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      // console.log(refreshToken);
+      console.log(profile);
+      // User.findOrCreate({ googleId: profile.id }, (err, user) => {
+      //   return done(err, user);
+      // });
+    }
+  )
+);
+
+// once we hit /auth/google - pop up google auth process
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+// code that we need exchange to info with google server
+// code is attached in query - req.query.code
+app.get("/auth/google/cb", passport.authenticate("google"));
 
 module.exports = app;
