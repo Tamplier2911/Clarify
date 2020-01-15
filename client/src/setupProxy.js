89145009385480a5b726d34a6e108c8bca6e135a
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2020-01-15 19:33:52 +0200

    Changed proxy, added references and icons to web manifest. Attempt to push to heroku.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,19 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/api", "/api/v1/", "/auth/google/", "/payment"], {
-      target: "http://localhost:5000"
-    })
+    proxy(
+      [
+        "/",
+        "/api",
+        "/api/v1/",
+        "/auth/google/",
+        "/payment",
+        "/service-worker.js"
+      ],
+      {
+        target: "http://localhost:5000"
+      }
+    )
   );
 };
 
